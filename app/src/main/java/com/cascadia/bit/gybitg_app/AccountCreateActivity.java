e6ff6307e8adf9210990608118b96b9f1ec06a42
Juanitaag1 <35508373+Juanitaag1@users.noreply.github.com>
2018-11-28 16:08:24 -0800

    updatedStrings

--- app/src/main/java/com/cascadia/bit/gybitg_app/AccountCreateActivity.java
+++ app/src/main/java/com/cascadia/bit/gybitg_app/AccountCreateActivity.java
@@ -16,14 +16,15 @@ public class AccountCreateActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //the floating action button mail below layout in create accountxml
+      //  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+       // fab.setOnClickListener(new View.OnClickListener() {
+         //   @Override
+        //    public void onClick(View view) {
+            //    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                     //   .setAction("Action", null).show();
+           // }
+      //  });
     }
 
 }
