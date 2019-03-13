e7c8cbaa06deb2df72be4e42c04e0e809ceed3bb
James H <jchayes86@gmail.com>
2019-03-13 15:14:16 -0700

    development+database/apprepo

---
+++ app/src/main/java/edu/cascadia/mobas/gybitg/viewmodel/StatFragmentViewModel.java
@@ -0,0 +1,12 @@
+package edu.cascadia.mobas.gybitg.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class StatFragmentViewModel extends AndroidViewModel {
+
+    public StatFragmentViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
